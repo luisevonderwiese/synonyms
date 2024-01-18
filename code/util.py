5a2235ae165683842b802e78884adc75ffb9d02b
luise <luise.haeuser@web.de>
2024-01-18 16:14:59 +0100

    Extend partitioning experiment

--- code/util.py
+++ code/util.py
@@ -6,6 +6,9 @@ def prefix(results_dir, row, experiment, run):
 def dist_dir(results_dir, row):
     return os.path.join(results_dir, "distances", "_".join([row["ds_id"], row["source"], row["ling_type"], row["family"]]))
 
+def dist_dir_partitioning(results_dir, row):
+    return os.path.join(results_dir, "distances_partitioning", "_".join([row["ds_id"], row["source"], row["ling_type"], row["family"]]))
+
 
 def partition_name(msa_type, model, gamma, mode):
     name = msa_type + "_" + model
--- experiment_partitioning.py
+++ experiment_partitioning.py
@@ -19,8 +19,44 @@ def run_raxml_ng(df):
         raxmlng.run_inference(row["msa_paths"]["bin"], "BIN+G", util.prefix(results_dir, row, "raxmlng", "bin"))
         partition_name = util.partition_name("bin", "BIN", True, "2")
         raxmlng.run_inference(row["msa_paths"]["bin"], row["partition_paths"][partition_name], util.prefix(results_dir, row, "raxmlng", partition_name))
-        partition_name = util.partition_name("bin", "BIN", True, "2")
+        partition_name = util.partition_name("bin", "BIN", True, "x")
         raxmlng.run_inference(row["msa_paths"]["bin"], row["partition_paths"][partition_name], util.prefix(results_dir, row, "raxmlng", partition_name))
+
+
+def calculate_distances(df):
+    metrics = ["rf", "gq"]
+    ref_tree_names = ["glottolog", "bin", "catg_bin", "catg_multi", "bin_BIN+G_2", "bin_BIN+G_x"]
+    d_io = DistanceMatrixIO(metrics, ref_tree_names)
+    for (i, row) in df.iterrows():
+        dist_dir = util.dist_dir_partitioning(results_dir, row)
+        if os.path.isfile(os.path.join(dist_dir, "matrix_rf.csv")) and os.path.isfile(os.path.join(dist_dir, "matrix_gq.csv")):
+            continue
+        ref_tree_paths = {}
+        ref_tree_paths["glottolog"] = row["glottolog_tree_path"]
+        ref_tree_paths["bin"] = raxmlng.best_tree_path(util.prefix(results_dir, row, "raxmlng", "bin"))
+        ref_tree_paths["catg_bin"] = raxmlng.best_tree_path(util.prefix(results_dir, row, "raxmlng", "catg_bin"))
+        ref_tree_paths["catg_multi"] = raxmlng.best_tree_path(util.prefix(results_dir, row, "raxmlng", "catg_multi"))
+        partition_name = util.partition_name("bin", "BIN", True, "2")
+        ref_tree_paths[partition_name] = raxmlng.best_tree_path(util.prefix(results_dir, row, "raxmlng", partition_name))
+        partition_name = util.partition_name("bin", "BIN", True, "x")
+        ref_tree_paths[partition_name] = raxmlng.best_tree_path(util.prefix(results_dir, row, "raxmlng", partition_name))
+        d_io.write_matrix(dist_dir, sampled_tree_paths, ref_tree_paths)
+
+
+def write_results_df(df):
+    sampled_difficulties = []
+    for i, row in df.iterrows():
+        alpha = raxmlng.alpha(util.prefix(results_dir, row, "raxmlng", "bin"))
+        df.at[i, "alpha"] = alpha
+        if alpha < 20:
+            df.at[i, "heterogenity"] = 1
+        else:
+            df.at[i, "heterogenity"] = 0
+        df.at[i, "difficulty"] = pythia.get_difficulty(util.prefix(results_dir, row, "pythia", "bin"))
+        df.at[i, "zero_base_frequency_bin"] = raxmlng.base_frequencies(util.prefix(results_dir, row, "raxmlng", "bin"))[0]
+    print_df = df[["ds_id", "source", "ling_type", "family", "alpha", "heterogenity", "difficulty", "zero_base_frequency_bin"]]
+    print(print_df)
+    print_df.to_csv(os.path.join(results_dir, "raxml_pythia_results_partitioning.csv"), sep = ";")
 
 
 
@@ -41,3 +77,5 @@ pd.set_option('display.max_rows', None)
 print(df)
 
 run_raxml_ng(df)
+calculate_distances(df)
+write_results_df(df)
