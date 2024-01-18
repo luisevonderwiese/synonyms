aa9c62ee724df23b97ae4955da989bc9005bbb7c
luise <luise.haeuser@web.de>
2024-01-18 14:46:50 +0100

    Add code for partitioning experiments

--- code/util.py
+++ code/util.py
@@ -5,3 +5,11 @@ def prefix(results_dir, row, experiment, run):
 
 def dist_dir(results_dir, row):
     return os.path.join(results_dir, "distances", "_".join([row["ds_id"], row["source"], row["ling_type"], row["family"]]))
+
+
+def partition_name(msa_type, model, gamma, mode):
+    name = msa_type + "_" + model
+    if gamma:
+        name += "+G"
+    name += "_" + mode
+    return name
---
+++ experiment_partitioning.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import numpy as np
+
+import lingdata.database as database
+
+import code.raxmlng as raxmlng
+import code.pythia as pythia
+import code.distances as distances
+from code.distances import DistanceMatrixIO
+import code.util as util
+
+
+
+
+
+def run_raxml_ng(df):
+    for (i, row) in df.iterrows():
+        raxmlng.run_inference(row["msa_paths"]["bin"], "BIN+G", util.prefix(results_dir, row, "raxmlng", "bin"))
+        partition_name = util.partition_name("bin", "BIN", True, "2")
+        raxmlng.run_inference(row["msa_paths"]["bin"], row["partition_paths"][partition_name], util.prefix(results_dir, row, "raxmlng", partition_name))
+        partition_name = util.partition_name("bin", "BIN", True, "2")
+        raxmlng.run_inference(row["msa_paths"]["bin"], row["partition_paths"][partition_name], util.prefix(results_dir, row, "raxmlng", partition_name))
+
+
+
+
+raxmlng.exe_path = "./bin/raxml-ng"
+pythia.raxmlng_path = "./bin/raxml-ng"
+pythia.predictor_path = "predictors/latest.pckl"
+distances.exe_path = "./bin/qdist"
+config_path = "synonyms_lingdata_config_partitioning.json"
+results_dir = "data/results"
+
+
+
+database.read_config(config_path)
+df = database.data()
+pd.set_option('display.max_rows', None)
+print(df)
+
+run_raxml_ng(df)
